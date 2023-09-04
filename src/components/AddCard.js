8c1f6293e30dff6ac8831db19093548fe2a6bf57
Martin Uche <martinuche19@outlook.com>
2023-09-04 18:23:45 -0500

    my updated commit...

--- src/components/AddCard.js
+++ src/components/AddCard.js
@@ -1,10 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useParams, useHistory } from 'react-router-dom';
 import { readDeck, createCard } from '../utils/api/index';
-
-
-
-
+import CardForm from './CardForm';
 
 function AddCard() {
   const { deckId } = useParams();
@@ -50,44 +47,7 @@ function AddCard() {
         </ol>
       </nav>
       <h1>{deck.name}: Add Card</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="front" className="form-label">
-            Front
-          </label>
-          <textarea
-            className="form-control"
-            id="front"
-            name="front"
-            rows="4"
-            placeholder="Front side of the card"
-            value={card.front}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="back" className="form-label">
-            Back
-          </label>
-          <textarea
-            className="form-control"
-            id="back"
-            name="back"
-            rows="4"
-            placeholder="Back side of the card"
-            value={card.back}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="button" className="btn btn-secondary mr-2" onClick={handleDone}>
-          Done
-        </button>
-        <button type="submit" className="btn btn-primary">
-          Save
-        </button>
-      </form>
+      <CardForm card={card} handleSubmit={handleSubmit} handleChange={handleChange} />
     </div>
   );
 }
