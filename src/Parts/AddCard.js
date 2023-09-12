5a50ab8f31194f1001d3490865b6aaa825222fa1
Martin Uche <martinuche19@outlook.com>
2023-09-12 18:27:05 -0500

    my latest update

--- src/Parts/AddCard.js
+++ src/Parts/AddCard.js
@@ -1,55 +1,49 @@
 import React , {useEffect , useState} from "react";
-import { useParams, Link, Switch , Route , useHistory } from "react-router-dom";
-import { readDeck, deleteDeck, createCard } from "../utils/api";
-import CardForm from "../components/CardForm";
+import { useParams , Link , Switch , Route , useHistory } from "react-router-dom";
+import { readDeck , deleteDeck, createCard } from "../utils/api";
 
-function AddCard() {
-  const { deckId } = useParams();
+const AddCard = () => {
+  const {deckId} = useParams();
+  const [deck , setDeck] = useState({});
+  const [front , setFront] = useState("");
+  const [back , setBack] = useState("");
   const history = useHistory();
-  const [deck, setDeck] = useState({ name: '' });
-  const [card, setCard] = useState({ front: '', back: '' });
 
-  // Load the deck
-  readDeck(deckId).then(setDeck);
+  const getDeck = async () => {
+    const data = await readDeck(deckId);
+    console.log(data)
+    setDeck(data);
+  }
 
-  const handleChange = (event) => {
-    setCard({
-      ...card,
-      [event.target.name]: event.target.value,
-    });
-  };
+  useEffect(() => {
+    getDeck();
+  }, [])
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    createCard(deckId, card).then(() => {
-      // Clear form
-      setCard({ front: '', back: '' });
-    });
-  };
+  return <div>
+  <Link to="/">Home</Link>
+      /
+      <Link to={`/decks/${deck.id}`}> {deck.name} </Link>
+      / AddCard
 
-  const handleDone = () => {
-    history.push(`/decks/${deckId}`);
-  };
-
-  return (
-    <div>
-      <nav aria-label="breadcrumb">
-        <ol className="breadcrumb">
-          <li className="breadcrumb-item">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="breadcrumb-item active" aria-current="page">
-            {deck.name}
-          </li>
-          <li className="breadcrumb-item active" aria-current="page">
-            Add Card
-          </li>
-        </ol>
-      </nav>
-      <h1>{deck.name}: Add Card</h1>
-      <CardForm card={card} handleSubmit={handleSubmit} handleChange={handleChange} />
-    </div>
-  );
+    <h1>{deck.name}: <span>Add Card</span></h1>
+      <form onSubmit={async (e)=>{
+      e.preventDefault()
+      await createCard(deck.id, {front , back});
+      setFront("");
+      setBack("");
+      }}>
+      <p>Front</p>
+      <textarea value={front} placeholder="Front side of card" onChange={(e)=>{
+        setFront(e.target.value)
+      }}></textarea>
+      <p>Back</p>
+      <textarea value={back} placeholder="Back side of card" onChange={(e)=>{
+        setBack(e.target.value)
+      }}></textarea>
+      <button onClick= {()=> history.push(`/decks/${deck.id}`)}>Done</button>
+      <button> Save </button>
+    </form>
+  </div>
 }
 
 export default AddCard;
